0240306e499094fc2cbf2708163bfdbf4d168d13
Shivam-Bhardwaj <sb6377@nyu.edu>
2019-03-22 15:25:35 -0400

    test

---
+++ code.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import os
+
+CI = os.listdir("camera_cal/")
+CI.sort()
+print(CI)
+nx = 9
+ny = 5
+
+img = cv2.imread("camera_cal/"+CI[1])
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret, corner = cv2.findChessboardCorners(gray, (nx, ny), None)
+
+cv2.drawChessboardCorners(img, (nx, ny), corner, ret)
+
+plt.imshow(img)
+plt.show()
